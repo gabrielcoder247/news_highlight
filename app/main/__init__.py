5115b5ce9b2ff4bb7c29cb6cf31478fedd3ebb6e
gabrielcoder <gabrielcoder247@gmail.com>
2018-06-19 23:39:26 +0300

    add api key in the congif.py file

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -3,11 +3,12 @@ from config import DevConfig
 
 #Initialize application
 
-app = Flask(__name__)
+app = Flask(__name__,instance_relative_config = True)
 
 #setting up configuration
 
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 
 from app import views
 
